3433f6f83d383f629d47aa07eb4c48288dfe6821
sobabag2021-create <sobabag2021@gmail.com>
2026-08-27 09:08:45 +0700

    Update page.tsx

--- soba-staff/app/page.tsx
+++ soba-staff/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "../../lib/supabase";
+import { supabase } from "../lib/supabase";
 
 type Employee = {
   id: string;
