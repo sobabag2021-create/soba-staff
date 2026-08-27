7631ac50ec0f2ab4247389b34458255ba61bb71c
sobabag2021-create <sobabag2021@gmail.com>
2026-08-27 08:46:21 +0700

    Update layout.tsx

--- soba-staff/app/layout.tsx
+++ soba-staff/app/layout.tsx
@@ -1,1 +1,18 @@
-import "./globals.css"; export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="vi"><body>{children}</body></html>}
+import "./globals.css";
+
+export const metadata = {
+  title: "SOBA STAFF",
+  description: "Quản lý nhân viên",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="vi">
+      <body>{children}</body>
+    </html>
+  );
+}
