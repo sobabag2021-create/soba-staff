ba344de5da06c821b35da07ebfdc487842791863
sobabag2021-create <sobabag2021@gmail.com>
2026-08-27 08:52:08 +0700

    Update page.tsx

--- soba-staff/app/admin/page.tsx
+++ soba-staff/app/admin/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "../lib/supabase";
+import { supabase } from "@/lib/supabase";
 
 export default function AdminPage() {
   const router = useRouter();
